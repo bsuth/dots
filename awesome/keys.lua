24d93c2c8c37ad9235c48eef5860a67dce1f4fd5
bsuth <bsuth@umich.edu>
2019-12-03 23:09:49 +0100

    better tag manipulation

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -15,6 +15,7 @@ local keys = {}
 ---------------------------------------
 
 local modkey = 'Mod4'
+local Alt = 'Mod1'
 
 
 ---------------------------------------
@@ -161,25 +162,49 @@ keys.global = gears.table.join(
 
     awful.key({ modkey, 'Control' }, 'h',
         function()
-            awful.screen.focus_by_direction('right')
+            awful.tag.incmwfact(-0.05)
         end,
-    {description = 'focus screen left'}),
+    {description = 'decrease master width'}),
 
     awful.key({ modkey, 'Control' }, 'j',
         function()
-            awful.screen.focus_by_direction('down')
+            awful.tag.incncol(-1, nil, true)
         end,
-    {description = 'focus screen down'}),
+    {description = 'decrease number of columns'}),
 
     awful.key({ modkey, 'Control' }, 'k',
         function()
-            awful.screen.focus_by_direction('up')
+            awful.tag.incncol(1, nil, true)
         end,
-    {description = 'focus screen up'}),
+    {description = 'increase number of columns'}),
 
     awful.key({ modkey, 'Control' }, 'l',
         function()
-            awful.screen.focus_by_direction('left')
+            awful.tag.incmwfact(0.05)
+        end,
+    {description = 'increase master width'}),
+
+    awful.key({ modkey, Alt }, 'h',
+        function()
+            awful.screen.focus_bydirection('right')
+        end,
+    {description = 'focus screen left'}),
+
+    awful.key({ modkey, Alt }, 'j',
+        function()
+            awful.screen.focus_bydirection('down')
+        end,
+    {description = 'focus screen down'}),
+
+    awful.key({ modkey, Alt }, 'k',
+        function()
+            awful.screen.focus_bydirection('up')
+        end,
+    {description = 'focus screen up'}),
+
+    awful.key({ modkey, Alt }, 'l',
+        function()
+            awful.screen.focus_bydirection('left')
         end,
     {description = 'focus screen right'}),
 
