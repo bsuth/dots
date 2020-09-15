f203c714f0594b4086a3fe81f23b5abe22343d89
Brian Sutherland <brian.sutherland@emma-sleep.com>
2020-09-15 09:41:38 +0200

    use brightnessctl over xbacklight -> faster + works on radeon

--- awesome/singletons/brightness.lua
+++ awesome/singletons/brightness.lua
@@ -9,18 +9,9 @@ local brightness = gears.object()
 gears.table.crush(brightness, { value = 0 }, true)
 
 awful.spawn.easy_async_with_shell(
-    [[ printf '%.0f' $(xbacklight) ]],
+    [[ echo $(( 100 * $(brightnessctl get) / $(brightnessctl max) )) ]],
     function(stdout, _, _, _) brightness:set(tonumber(stdout)) end
 )
-
--- -------------------------------------------------------------------------
--- TODO: Add notification
---
--- Note: xbacklight is SLOW. If we wait for xbacklight to finish before
--- sending the notification, there is a considerable amount of lag. Thus,
--- we precompute the new brightness percentage when sending the notification
--- and only change it after.
--- -------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
 -- API
@@ -32,7 +23,7 @@ end
 
 function brightness:set(value)
     value = math.min(math.max(0, value), 100)
-    awful.spawn(('xbacklight -set %s'):format(value))
+    awful.spawn(('brightnessctl set %s%%'):format(value))
     self.value = value
     self:emit_signal('update', value)
 end
