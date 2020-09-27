324737b9adb385ec8d66a6dea2fcf72cd0bd8dd3
Brian Sutherland <bsuth701@gmail.com>
2020-09-27 21:44:27 +0200

    update build root script

--- scripts/build/root.sh
+++ scripts/build/root.sh
@@ -2,15 +2,15 @@
 
 # ------------------------------------------------------------------------------
 # README
-#
+
 # This is a script to setup my daily driver environment on a machine. It assumes
 # that the current user is `root`.
 # ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# ANSI COLOR CODES
-# ------------------------------------------------------------------------------
+# Status
+status=0
 
+# ANSI color codes
 RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
 NC="$(tput sgr0)"
@@ -19,14 +19,22 @@ NC="$(tput sgr0)"
 # HELPERS
 # ------------------------------------------------------------------------------
 
+function _report_status_() {
+    case $status in
+	0) echo -e "\n${GREEN}--> Success! <--${NC}\n" ;;
+	1) echo -e "\n${RED}--> Failed <--${NC}\n" ;;
+	2|*) echo -e "\n${RED}--> Skipped <--${NC}\n" ;;
+    esac	    
+}
+
 function _yesno_() {
     while true; do
         printf "$1 (y/n): "
         read yn
 
         case "$yn" in
-            y) return 0 ;;
-            n) return 1 ;;
+            y|Y) return 0 ;;
+            n|N) return 1 ;;
             *) echo -e "${RED}Invalid input${NC}\n"
         esac
     done
@@ -41,15 +49,88 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
-echo -e "${GREEN}==================================${NC}"
-echo -e "${GREEN}========== Global Setup ==========${NC}"
-echo -e "${GREEN}==================================${NC}\n"
+# Newline for readability
+echo
+
+# --------------------------------------
+# Setup Wifi
+# --------------------------------------
+
+function _setup_wifi_() {
+    if ! _yesno_ "Setup wifi?"; then status=2; return; fi
+
+    # See here for a list of all interface prefixes:
+    # https://www.freedesktop.org/software/systemd/man/systemd.net-naming-scheme.html
+    found_interface=0
+
+    for interface in $(ls /sys/class/net); do
+        if [[ ${interface:0:2} == "wl" ]]; then
+            echo -e "\nFound WLAN interface: $interface"
+            if ! _yesno_ "Use this?"; then continue; fi 
+
+	    while : ; do
+		echo
+                read -p "SSID: " ssid
+                read -p "Password: " -s pass
+		echo -e "\n"
+
+		if wpa_passphrase "$ssid" "$pass" >/dev/null; then
+		    printf "Saving configuration and setting up service..."
+                    wpa_passphrase "$ssid" "$pass" > "/etc/wpa_supplicant/wpa_supplicant-${interface}.conf"
+                    systemctl enable "wpa_supplicant@${interface}.service" >/dev/null 2>&1
+                    systemctl restart "wpa_supplicant@${interface}.service" >/dev/null 2>&1
+		    echo "${GREEN}done${NC}"
+
+		    while : ; do
+		        printf "Checking connection..."
+
+			# Give some time for the network to connect
+			sleep 2
+
+		        if ping -c 1 8.8.8.8 -W 10 >/dev/null 2>&1; then
+		    	    echo "${GREEN}done${NC}"
+		            status=0
+		            return
+		        fi
+    
+		    	echo "${RED}failed${NC}"
+		        echo -e "\n${RED}Failed to ping 8.8.8.8${NC}"
+		    	if ! _yesno_ "Try again?"; then break; fi
+		    done
+
+		    echo
+		    printf "Configuration aborted. Reverting changes..."
+                    rm "/etc/wpa_supplicant/wpa_supplicant-${interface}.conf"
+                    systemctl stop "wpa_supplicant@${interface}.service" >/dev/null 2>&1
+                    systemctl disable "wpa_supplicant@${interface}.service" >/dev/null 2>&1
+		    echo "${GREEN}done${NC}"
+
+		    if ! _yesno_ "Try different credentials?"; then break; fi
+		else
+		    echo "${RED}Invalid parameters (wpa_passphrase returned non-zero exit status)${NC}"
+		    if ! _yesno_ "Try again?"; then break; fi
+		fi
+	    done
+        fi
+    done
+
+    echo -e "\n${RED}Error: Failed to find WLAN interface.${NC}"
+
+    if ! _yesno_ "Continue?"; then
+	status=1; _report_status_
+	exit 0
+    else
+	status=2
+    fi
+}
+
+echo -e "${GREEN}=== Setting up wifi ===${NC}\n"
+_setup_wifi_
+_report_status_
 
 # --------------------------------------
 # Install Packages
 # --------------------------------------
-
-echo -e "${GREEN}=== Installing packages ===${NC}\n"
 
 PACKAGES=(
     # System
@@ -84,38 +165,37 @@ PACKAGES=(
     compton
 )
 
-apt install "${PACKAGES[@]}"
+function _install_packages_() {
+    if ! _yesno_ "Install packages?"; then status=2; return; fi
+
+    while : ; do
+        if apt install "${PACKAGES[@]}"; then
+	    status=0
+	    return
+        else
+	    if ! _yesno_ "Try again?"; then break; fi
+        fi
+    done
+
+    status=2
+}
+
+echo -e "${GREEN}=== Installing packages ===${NC}\n"
+_install_packages_
+_report_status_
 
 # --------------------------------------
 # Update sudoers
 # --------------------------------------
 
-echo -e "${GREEN}=== Adding bsuth to sudoers ===${NC}\n"
-echo -e "Adding bsuth to sudoers\n"
-addmod -a -G bsuth sudo
+function _setup_sudoers_() {
+    if usermod -a -G sudo bsuth; then
+        status=0
+    else
+        status=1
+    fi
+}
 
-# --------------------------------------
-# Setup Wifi
-# --------------------------------------
-
-echo -e "${GREEN}=== Setting up wifi ===${NC}\n"
-
-if _yesno_ "Setup wifi?"; then
-    # See here for a list of all interface prefixes:
-    # https://www.freedesktop.org/software/systemd/man/systemd.net-naming-scheme.html
-
-    for interface in "$(ls /sys/class/net)"; do
-        if [[ ${interface:0:2} == "wl" ]]; then
-            echo "Found WLAN interface: $interface"
-
-            if __yesno__ "Use this?"; then
-                printf "SSID: "; read $ssid
-                printf "Password: "; read -s $pass
-                wpa_passphrase "$ssid" "$pass" > "/etc/wpa_supplicant/wpa_supplicant-${interface}.conf"
-                systemctl enable "wpa_supplicant@${interface}.service"
-                systemctl start "wpa_supplicant@${interface}.service"
-                break
-            fi
-        fi
-    done
-fi
+echo -e "${GREEN}=== Adding bsuth to sudoers ===${NC}"
+_setup_sudoers_
+_report_status_
