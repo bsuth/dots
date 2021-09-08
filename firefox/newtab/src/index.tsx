5be890212b591caf5211e7231603e002e14fdf3b
bsuth <bsuth701@gmail.com>
2021-09-08 17:38:57 +0900

    customize firefox woo!

--- firefox/newtab/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import './index.scss';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-
-render(
-  <BrowserRouter children={[<Navbar />]} />,
-  document.getElementById('root'),
-);
