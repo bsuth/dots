467ab6f16ec795059f3eea6bd373a71f05a98ea7
bsuth <bsuth701@gmail.com>
2021-10-24 20:46:28 +0900

    custom dmenu commands

--- awesome/navbar/dmenu.lua
+++ awesome/navbar/dmenu.lua
@@ -2,6 +2,36 @@ local awful = require('awful')
 local wibox = require('wibox')
 
 -- -----------------------------------------------------------------------------
+-- Commands
+-- -----------------------------------------------------------------------------
+
+local DMENU_COMMANDS = {
+  flameshot = function()
+    awful.spawn('flameshot gui')
+  end,
+  gpick = function()
+    awful.spawn.with_shell('gpick -s -o | tr -d "\n" | xclip -sel c')
+  end,
+  inkscape = function()
+    awful.spawn('inkscape')
+  end,
+  simplescreenrecorder = function()
+    awful.spawn('simplescreenrecorder')
+  end,
+  term = function()
+    awful.spawn('st -e nvim -c ":Dirvish"')
+  end,
+  vivaldi = function()
+    awful.spawn('vivaldi-stable')
+  end,
+}
+
+local DMENU_COMMAND_KEYS = {}
+for key, _ in pairs(DMENU_COMMANDS) do
+  DMENU_COMMAND_KEYS[#DMENU_COMMAND_KEYS + 1] = key
+end
+
+-- -----------------------------------------------------------------------------
 -- Dmenu
 -- -----------------------------------------------------------------------------
 
@@ -10,7 +40,19 @@ local Dmenu = setmetatable({}, {
     local prompt = awful.widget.prompt({
       prompt = '',
       font = 'Fredoka One 14',
-      exe_callback = awful.spawn,
+      completion_callback = function(currentCmd, cursorPos, nComp)
+        return awful.completion.generic(
+          currentCmd,
+          cursorPos,
+          nComp,
+          DMENU_COMMAND_KEYS
+        )
+      end,
+      exe_callback = function(cmd)
+        if type(DMENU_COMMANDS[cmd]) == 'function' then
+          DMENU_COMMANDS[cmd]()
+        end
+      end,
       done_callback = function()
         navbar:setMode('tabs')
       end,
