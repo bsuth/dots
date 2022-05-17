6dd2423b8dacc6d281da650106096c10f4b79967
brian.sutherland <brian.sutherland@mujin.co.jp>
2022-05-17 17:47:02 +0900

    stylelint on save

--- nvim/lua/language-support.lua
+++ nvim/lua/language-support.lua
@@ -15,31 +15,6 @@ map('n', "'q", ':lua vim.diagnostic.setloclist()<cr>')
 map('n', "'s", ':lua vim.lsp.buf.rename()<cr>')
 map('n', "'c", ':lua vim.lsp.buf.code_action()<cr>')
 
-map(
-  'i',
-  '<Tab>',
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
-  { noremap = false, expr = true }
-)
-map(
-  's',
-  '<Tab>',
-  "vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>'",
-  { noremap = false, expr = true }
-)
-map(
-  'i',
-  '<S-Tab>',
-  "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<Tab>'",
-  { noremap = false, expr = true }
-)
-map(
-  's',
-  '<S-Tab>',
-  "vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<Tab>'",
-  { noremap = false, expr = true }
-)
-
 -- map('n', "'i", ':lua vim.lsp.buf.implementation()<cr>')
 -- map('n', "'k", ':lua vim.lsp.buf.signature_help()<cr>')
 -- map('n', "<space>D", ':lua vim.lsp.buf.type_definition()<cr>')
@@ -75,11 +50,6 @@ require('nvim-treesitter.configs').setup({
 local cmp = require('cmp')
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
-    end,
-  },
   mapping = {
     ['<c-d>'] = cmp.mapping.scroll_docs(-4),
     ['<c-f>'] = cmp.mapping.scroll_docs(4),
@@ -89,7 +59,6 @@ cmp.setup({
   },
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },
     { name = 'buffer' },
     { name = 'path' },
   },
@@ -111,8 +80,13 @@ local lspServers = {
   graphql = {},
   gopls = {},
   eslint = {},
-  stylelint_lsp = {},
   tsserver = {},
+  stylelint_lsp = {
+    settings = {
+      autoFixOnSave = true,
+      autoFixOnFormat = true,
+    }
+  },
   jsonls = {
     filetypes = { 'json', 'jsonc' },
     settings = {
@@ -155,16 +129,20 @@ for server, config in pairs(lspServers) do
   }, config))
 end
 
-augroup('bsuth-fix-on-save', {
-  autocmd('BufWritePre', 'EslintFixAll', BUFFER_PATTERNS.js),
-})
-
 -- -----------------------------------------------------------------------------
 -- Formatter
 -- https://github.com/mhartington/formatter.nvim
 -- -----------------------------------------------------------------------------
 
 local formatter = require('formatter')
+
+function applyStylelint()
+  return {
+    exe = 'npx stylelint --quiet --fix',
+    args = { nvim_buf_get_name(0) },
+    stdin = false,
+  }
+end
 
 function applyPrettier()
   return {
@@ -181,6 +159,10 @@ function applyStylua()
     stdin = false,
   }
 end
+
+augroup('bsuth-fix-on-save', {
+  autocmd('BufWritePre', 'EslintFixAll', BUFFER_PATTERNS.js),
+})
 
 augroup('bsuth-format-on-save', {
   -- autocmd('BufWritePost', 'FormatWrite', { '*.lua' }),
@@ -199,8 +181,8 @@ formatter.setup({
     typescript = { applyPrettier },
     typescriptreact = { applyPrettier },
     graphql = { applyPrettier },
-    css = { applyPrettier },
-    scss = { applyPrettier },
-    less = { applyPrettier },
+    css = { applyStylelint, applyPrettier },
+    scss = { applyStylelint, applyPrettier },
+    less = { applyStylelint, applyPrettier },
   },
 })
