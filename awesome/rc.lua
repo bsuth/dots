152d67d5a37fbd5bf5c1f5009409d117b482341f
bsuth <bsuth701@gmail.com>
2022-12-08 20:23:02 +0900

    create awesomewm core modules

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,2 +1,13 @@
+-- Autofocus another client when the current one is closed
+require('awful/autofocus')
+
+-- load erde
 require('erde').load()
-require('init')
+
+-- load modules
+require('core.bindings')
+require('core.layout')
+require('core.rules')
+require('core.theme')
+require('core.misc')
+require('clientBuffer')
