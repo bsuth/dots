53977068afe9ae58c6855a264a22183171d8a863
bsuth <bsuth701@gmail.com>
2022-12-10 14:59:31 +0900

    finish modularizing awesomewm core

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,15 +1,2 @@
--- Autofocus another client when the current one is closed
-require('awful/autofocus')
-
--- load erde
 require('erde').load()
-
--- load modules
-require('core.bindings')
-require('core.layout')
-require('core.rules')
-require('core.theme')
-require('core.misc')
-require('clientBuffer')
-require('dmenu')
-require('statusMenu')
+require('init')
