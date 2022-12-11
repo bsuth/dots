c03cb9042ebc6c3bc66ec7ca9411b1c9be8772bc
bsuth <bsuth701@gmail.com>
2022-12-11 11:48:38 +0900

    remove lualine separators

--- nvim/init.lua
+++ nvim/init.lua
@@ -408,8 +408,9 @@ require('lualine').setup({
   options = {
     icons_enabled = false,
     theme = 'onedark',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
   },
-
   sections = {
     lualine_x = {},
   },
