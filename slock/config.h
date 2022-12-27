690a6297e2aa452274e3dd6ac79f209f3693d089
bsuth <bsuth701@gmail.com>
2022-12-27 13:03:18 +0900

    update

--- slock/config.h
+++ slock/config.h
@@ -3,14 +3,14 @@ static const char *user  = "bsuth";
 static const char *group = "bsuth";
 
 static const char *colorname[NUMCOLS] = {
-	[BG] =     "#101012",     /* background */
+	[BG] =     "#101012",   /* background */
 	[INIT] =   "#4f525c",   /* after initialization */
 	[INPUT] =  "#57a5e5",   /* during input */
 	[FAILED] = "#de5d68",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* size in px of cube */
 static const int squaresize = 50;
