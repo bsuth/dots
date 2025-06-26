9eedb8aad023bdd1e4377173157bbbec8150127e
bsuth <bsuth701@gmail.com>
2025-06-26 19:52:26 +0900

    remove unused dot files

--- catnip/deprecated/ffi/gio/init.lua
+++
@@ -1,8 +0,0 @@
-local ffi = require("ffi")
-
-require('ffi.gobject')
-require('ffi.gio.gio_types')
-require('ffi.gio.gio_enums')
-require('ffi.gio.g_dbus_connection')
-
-return ffi.load("libgio-2.0.so")
--- catnip/deprecated/ffi/glib/g_error.lua
+++
@@ -1,8 +0,0 @@
-local ffi = require("ffi")
-
--- https://docs.gtk.org/glib/struct.Error.html
--- /usr/include/glib-2.0/glib/gerror.h
-
-ffi.cdef([[
-typedef struct {} GError;
-]])
